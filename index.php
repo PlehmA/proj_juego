d15c8e83c9e09b5cb4c8d49bdaf300cad2bc2e02
Andres Plehm <polachgg@gmail.com>
2016-07-25 20:35:33 -0300

    Trying to do a game
    Cant put an image -.-

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
     <title>-=Orchish=-</title>
   </head>
   <body>
+    <div id="wrapper">
     <h1>Orchish</h1>
     <h2>¡Mundo orco!</h2>
     <nav>
@@ -15,6 +17,23 @@
         <li><a href="#">Quien soy</a></li>
       </ul>
     </nav>
+    <div id="loginFormWrapper">
+<form>
+<legend>Logeate</legend>
+                <input name="user" type="text">
+                <input name="pass" type="text">
+                <input type="submit" value="Logearse">
+            </form>
+</div>
+<div id="columnaWrapper">
+<div class="columna">
+Esta es mi columna 1</div>
+<div class="columna">
+Esta es mi columna 2</div>
+<div class="columna" id="supercolumna">
+Esta es mi supercolumna 3</div>
+</div>
+</div>
     <?php
 
 
